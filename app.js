1798cee9d2f78ff0951635f798f9e4a545a5f93a
Kelly <kellyc@evollve.co>
2020-09-12 19:55:44 -0500

    saludame-2

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+function capitalize (string) {
+return string.charAt(0).toUpperCase()
+}
+
+app.get('/makers/:name', (req, res) => {
+    const { name } = req.params;
+      let template = " Hola ";
+
+      template += capitalize (name) + "!";
+
+      res.send(`<h1> ${template}</h1>`);
+  });
+  
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
